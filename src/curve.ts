6767169596d91617f81b760b1f5fafd46d506ded
very-amused <very-amused@pm.me>
2022-06-16 16:57:01 -0400

    Configure eslint and typescript

---
+++ src/curve.ts
@@ -0,0 +1,19 @@
+import { createWriteStream, type WriteStream } from 'fs'
+
+// Simple wrapper class over a csv file
+export class BenchmarkCurve {
+  private stream: WriteStream
+
+  constructor(file: `${string}.csv`) {
+    this.stream = createWriteStream(file, { flags: 'a' })
+  }
+
+  push(time: number, value: number): void {
+    this.stream.write(`${time}, ${value}\n`)
+  }
+
+  /** Close the curve's output file. Must be called to save buffered output. */
+  save(): void {
+    this.stream.close()
+  }
+}
