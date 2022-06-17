acef614857ec990cca99dacb351d107cc2a1864b
very-amused <very-amused@pm.me>
2022-06-17 17:23:23 -0400

    Extended benchmark format

--- src/curve.ts
+++ src/curve.ts
@@ -1,11 +1,18 @@
-import { createWriteStream, type WriteStream } from 'fs'
+import { type WriteStream, createWriteStream, mkdirSync } from 'fs'
+import { dirname } from 'path'
 
 // Simple wrapper class over a csv file
 export class BenchmarkCurve {
   private stream: WriteStream
 
-  constructor(file: `${string}.csv`) {
+  constructor(file: `${string}.csv`, variableHeader: string) {
+    const dir = dirname(file)
+    mkdirSync(dir, {
+      recursive: true
+    })
     this.stream = createWriteStream(file, { flags: 'w' })
+    // Add CSV headers
+    this.stream.write(`Login Time (ms), ${variableHeader}\n`)
   }
 
   push(time: number, value: number): void {
--- src/memory.ts
+++ src/memory.ts
@@ -11,50 +11,101 @@ const wasm = {
   argon2Simd: readFileSync(`${wasmBasePath}/argon2-simd.wasm`)
 }
 
+enum BenchmarkTypes {
+  Memory,
+  Time,
+  Parallelism
+}
+
 type Benchmark = {
-  // Constants
+  type: BenchmarkTypes
+  // Constants (used as minimum value for the field being benchmarked)
   time: number
   parallelism: number
+  memory: number // in KiB
   simd: boolean
   password: string
 
-  // Min and max memory values, in KiB
-  minMemory: number
-  maxMemory: number
+  // Max value of the field being benchmarked
+  max: number
 
   // The resolution of the generated curve, equal to the number of tests to be run - 1
   segments: number
 }
-export type MemoryBenchmark = Benchmark
 
 async function runBenchmark(b: Benchmark): Promise<void> {
   // Initialize the curve output
-  const dir = `t${b.time}_p${b.parallelism}`
-  const filename = `${b.minMemory}-${b.maxMemory}` + (b.simd ? '_simd' : '')
-  const curve = new BenchmarkCurve(`results/memory/${dir}/${filename}.csv`)
+  let
+    dir: string,
+    file: string,
+    variableHeader: string,
+    min: number
+  // Label the output curve using the constant values, and place it in a folder for the variable being measured
+  switch (b.type) {
+  case BenchmarkTypes.Memory:
+    dir = `results/memory/t${b.time}_p${b.parallelism}`
+    file = `${b.memory}-${b.max}`
+    variableHeader = 'Memory Parameter (KiB)'
+    min = b.memory
+    break
+  case BenchmarkTypes.Time:
+    dir = `results/time/m${b.memory}_p${b.parallelism}`
+    file = `${b.time}-${b.max}`
+    variableHeader = 'Time Parameter (iterations)'
+    min = b.time
+    break
+  case BenchmarkTypes.Parallelism:
+    dir = `results/parallelism/t${b.time}_m${b.memory}`
+    file = `${b.parallelism}-${b.max}`
+    variableHeader = 'Parallelism Parameter (threads)'
+    min = b.parallelism
+    break
+  }
+  // Suffix with whether reference or SIMD memory handling is used
+  if (b.simd) {
+    file += '_simd'
+  }
+
+  
+  const curve = new BenchmarkCurve(`${dir}/${file}.csv`, variableHeader)
 
   // Generate a random salt
   const salt = crypto.getRandomValues(new Uint8Array(16))
 
-  const increment = Math.floor((b.maxMemory - b.minMemory) / b.segments)
+  const increment = Math.floor((b.max - min) / b.segments)
   outer:
   for (let i = 0; i <= b.segments; i++) {
-    const memory = i === b.segments ? b.maxMemory : b.minMemory + (i * increment)
-    console.log(`[${i+1}/${b.segments+1}] Benchmarking m = ${memory}`)
+    const v = i === b.segments ? b.max : min + (i * increment)
+    console.log(`[${i+1}/${b.segments+1}] ${benchmarkMessage(v, b.type)}`)
+
+    // Prepare parameters
+    const params: Parameters<Argon2['hashPassword']>[0] = {
+      password: b.password,
+      salt,
+      timeCost: b.time,
+      memoryCost: b.memory,
+      parallelism: b.parallelism,
+      hashLen: 32
+    }
+
+    // Set the variable being benchmarked
+    switch (b.type) {
+    case BenchmarkTypes.Memory:
+      params.memoryCost = v
+      break
+    case BenchmarkTypes.Time:
+      params.timeCost = v
+      break
+    case BenchmarkTypes.Parallelism:
+      params.parallelism = v
+    }
 
     // Benchmark the time required to initialize argon2-wasm and run argon2 twice
     const start = performance.now()
     const argon2 = new Argon2(b.simd ? wasm.argon2Simd : wasm.argon2)
     await argon2.instantiate()
     for (let run = 0; run < 2; run++) {
-      const code = argon2.hashPassword({
-        password: b.password,
-        salt,
-        timeCost: b.time,
-        memoryCost: memory,
-        parallelism: b.parallelism,
-        hashLen: 32
-      })
+      const code = argon2.hashPassword(params)
       if (code !== 0) {
         console.error(`Failed with code ${code}`)
         curve.save()
@@ -63,19 +114,32 @@ async function runBenchmark(b: Benchmark): Promise<void> {
     }
     const elapsed = performance.now() - start
     console.log(`Done in ${(elapsed / 1000).toFixed(3)}s.\n`)
-    curve.push(elapsed, memory)
+    curve.push(elapsed, v)
   }
   curve.save()
 }
 
+// The message to be printed to the console for a benchmark
+function benchmarkMessage(v: number, type: BenchmarkTypes): string {
+  switch (type) {
+  case BenchmarkTypes.Memory:
+    return `m = ${v}`
+  case BenchmarkTypes.Time:
+    return `t = ${v}`
+  case BenchmarkTypes.Parallelism:
+    return `p = ${v}`
+  }
+}
+
 await runBenchmark({
-  time: 1,
-  parallelism: 1,
-  simd: false,
+  type: BenchmarkTypes.Memory,
+  time: 3,
+  memory: 1024,
+  parallelism: 8,
+  simd: true,
   password: 'samplepassword',
 
-  minMemory: 10 * 1024,
-  maxMemory: 1024 * 1024,
+  max: 1024 * 1024,
 
-  segments: 10
+  segments: 20
 })
